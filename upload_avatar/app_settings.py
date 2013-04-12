7eb3937580b2fef179ebc343e2e8f53e9aacde92
yueyoum <yueyoum@gmail.com>
2013-04-12 23:34:14 +0800

    more comments

--- upload_avatar/app_settings.py
+++ upload_avatar/app_settings.py
@@ -3,10 +3,10 @@ import os
 
 from django.conf import settings
 
-# where the uploaded image store
+# where the uploaded image store, absolute path
 UPLOAD_AVATAR_UPLOAD_ROOT = settings.UPLOAD_AVATAR_UPLOAD_ROOT
 
-# where the cropped avatar store
+# where the cropped avatar store, absolute path
 UPLOAD_AVATAR_AVATAR_ROOT = settings.UPLOAD_AVATAR_AVATAR_ROOT
 
 
@@ -17,9 +17,11 @@ if not os.path.isdir(UPLOAD_AVATAR_AVATAR_ROOT):
 
 
 # URL prefix for the original uploaded image
+# e.g. uploadedimage/ or image/ ...
 UPLOAD_AVATAR_URL_PREFIX_ORIGINAL = settings.UPLOAD_AVATAR_URL_PREFIX_ORIGINAL
 
 # URL prefix for the cropped, real avatar
+# e.g. avatar/
 UPLOAD_AVATAR_URL_PREFIX_CROPPED = settings.UPLOAD_AVATAR_URL_PREFIX_CROPPED
 
 # Yes, you noticed there are two URLs,
@@ -35,13 +37,16 @@ UPLOAD_AVATAR_MAX_SIZE = getattr(settings, 'UPLOAD_AVATAR_MAX_SIZE', 1024 * 1024
 # You should custom the test function.
 # test function take 'request' as the only argument.
 # It controls whether this request is valid and call the view function
-# Default test function as you see, just ensure that the 'POST' method
-# you maybe do more tests such as user is logged, or some also.
+# Default test function as you see, just ensure that the 'POST' method and verify the user
+# you can do more tests.
 UPLOAD_AVATAR_TEST_FUNC = getattr(settings, 'UPLOAD_AVATAR_TEST_FUNC', None)
 if UPLOAD_AVATAR_TEST_FUNC is None:
     UPLOAD_AVATAR_TEST_FUNC = lambda request: request.method == 'POST' and \
                               request.user.is_authenticated()
-    
+
+# If you are using custom user, and the user object isn't `request.user`
+# you should define this function, for me to know how get the uid from request
+# this function take `request` as argument, and return the current logged user's id
 UPLOAD_AVATAR_GET_UID_FUNC = getattr(settings, 'UPLOAD_AVATAR_GET_UID_FUNC', None)
 if UPLOAD_AVATAR_GET_UID_FUNC is None:
     UPLOAD_AVATAR_GET_UID_FUNC = lambda request: request.user.id
@@ -49,16 +54,31 @@ if UPLOAD_AVATAR_GET_UID_FUNC is None:
     
     
 
+# How many different size your wanna resize.
+# NOTICE, the value MUST be list, even if there are only one size.
 UPLOAD_AVATAR_RESIZE_SIZE = getattr(settings, 'UPLOAD_AVATAR_CROP_SIZE', [50,])
+
+# Avatar default size which will be shown in you website,
+# this is for call user.get_avatar_path(), user.get_avatar_url() more convenient
 UPLOAD_AVATAR_DEFAULT_SIZE = getattr(settings, 'UPLOAD_AVATAR_DEFAULT_SIZE', 50)
 
+# Avatar format, you can also choose: jpep, gif...
 UPLOAD_AVATAR_SAVE_FORMAT = getattr(settings, 'UPLOAD_AVATAR_SAVE_FORMAT', 'png')
+if UPLOAD_AVATAR_SAVE_FORMAT == 'jpg':
+    UPLOAD_AVATAR_SAVE_FORMAT = 'jpeg'
+
 UPLOAD_AVATAR_SAVE_QUALITY = getattr(settings, 'UPLOAD_AVATAR_SAVE_QUALITY', 90)
 
 
+# Whethe delete the uploaded original image after avatar cropped, default is True
 UPLOAD_AVATAR_DELETE_ORIGINAL_AFTER_CROP = getattr(settings, 'UPLOAD_AVATAR_DELETE_ORIGINAL_AFTER_CROP', True)
 
 
+
+# Bellow settings are for web layout and text shown in web or javascript alert
+# Why not using i18n?
+# first, i18n will reduce django performance
+# second, If this app using i18n, But your project not, what you will do? Enable i18n JUST for this app?
 
 
 UPLOAD_AVATAR_WEB_LAYOUT = {
@@ -67,7 +87,11 @@ UPLOAD_AVATAR_WEB_LAYOUT = {
     'preview_areas': [
         {
             'size': 50,
-            'text': 'Preview'
+            'text': 'Small Preview'
+        },
+        {
+            'size': 120,
+            'text': 'Large Preview'
         },
     ]
 }
